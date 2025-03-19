b9cccd481466dfacc69b50e6cd0b6ae61c6f071a
zikou2025 <zikouhoudache@gmail.com>
2025-03-19 14:50:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
 import Index from "./pages/Index";
+import Timeline from "./pages/Timeline";
+import MindMap from "./pages/MindMap";
+import Geography from "./pages/Geography";
+import Characters from "./pages/Characters";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,8 +20,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Index />} />
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/mindmap" element={<MindMap />} />
+            <Route path="/geography" element={<Geography />} />
+            <Route path="/characters" element={<Characters />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
